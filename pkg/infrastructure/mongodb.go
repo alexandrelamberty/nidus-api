0b02f3743af51b5920c3cf027980f7a085685847
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-08-23 12:43:08 +0200

    Consolidate database connection and disconnection

--- pkg/infrastructure/mongodb.go
+++ pkg/infrastructure/mongodb.go
@@ -12,14 +12,19 @@ import (
 
 // FIXME: Use environment variables
 func MongoDBConnection() (*mongo.Database, error) {
-	//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	clientOptions := options.Client().ApplyURI(os.Getenv("DATABASE_URI")).SetServerSelectionTimeout(5 * time.Second)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		fmt.Printf(err.Error())
-		//cancel()
+		fmt.Println(err.Error())
 		return nil, err
 	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 	db := client.Database("nidus")
 	return db, nil
 }
