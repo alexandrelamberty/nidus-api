7e3f1a3d2fadfd83a554e2effae91ace9a08e5bd
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-01-29 16:01:52 +0100

    chore: duplicate struct key

--- pkg/domain/device.go
+++ pkg/domain/device.go
@@ -10,9 +10,9 @@ type Device struct {
 	Mac            string               `json:"mac" bson:"mac,omitempty"`
 	Ip             string               `json:"ip" bson:"ip,omitempty"`
 	CapabilityIDs  []primitive.ObjectID `json:"capability_ids" bson:"capability_ids,omitempty"`
-	Capabilities   []*Capability        `json:"capabilities" bson:"-",omitempty`
+	Capabilities   []*Capability        `json:"capabilities" bson:"capabilities,omitempty"`
 	ZoneID         primitive.ObjectID   `json:"zone_id" bson:"zone_id,omitempty"`
-	Zone           *Zone                `json:"zone" bson:"-",omitempty`
+	Zone           *Zone                `json:"zone" bson:"zone,omitempty"`
 	Paired         bool                 `json:"paired" bson:"paired,omitempty"`
 	Zoned          bool                 `json:"zoned" bson:"zoned,omitempty"`
 	LastConnection string               `json:"last_connection" bson:"last_connection,omitempty"`
