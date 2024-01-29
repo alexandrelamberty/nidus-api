02a0e95b8c1e775219f7f9d0677e50079b5848b1
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-01-29 16:16:36 +0100

    feat: add Metadata validation

--- internal/requests/measurement.go
+++ internal/requests/measurement.go
@@ -1,13 +1,16 @@
 package requests
 
 import (
-	"nidus-server/pkg/domain"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Metadata struct {
+	DeviceID primitive.ObjectID `json:"device_id,omitempty" validate:"required,mongodb"`
+	Type     string             `json:"type" validate:"required,min=4,max=32"`
+}
+
 type CreateMeasurementRequest struct {
-	Metadata  domain.Metadata    `json:"metadata" validate:"required"`
+	Metadata  Metadata           `json:"metadata" validate:"required"`
 	Value     float32            `json:"value" validate:"required, number"`
 	Timestamp primitive.DateTime `json:"timestamp" validate:"required"`
 }
