49eb1c4ea3b01dc353ddf211eb8c00d635aab468
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-01-29 16:15:55 +0100

    feat: add UpdateWidget request

--- internal/requests/widget.go
+++ internal/requests/widget.go
@@ -1,9 +1,14 @@
 package requests
 
+// FIXME: This id done internally in the server. Remove it in the future.
 type CreateWidget struct {
 	Name     string `json:"name" validate:"required,min=4,max=32"`
 	DeviceID string `json:"device_id" validate:"required,hexadecimal,len=24"`
 	// FIXME: capability ?
 	Type    string `json:"type" validate:"required,min=4,max=32"`
-	Enabled bool   `json:"enabled" validate:"required, boolean"`
+	Enabled bool   `json:"enabled" validate:"required, boolean"` // FIXME: false by default
+}
+
+type UpdateWidget struct {
+	Enabled bool `json:"enabled" validate:"required, boolean"`
 }
